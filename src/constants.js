2ca291f1a8249a8b936f77b94f976cade8723a38
kojesung <js95112345@gmail.com>
2026-01-07 18:23:15 +0900

    feat(constants): 월별 일자 상수 선언

--- src/constants.js
+++ src/constants.js
@@ -30,3 +30,18 @@ export const PUBLIC_HOLIDAY = [
     { month: 10, date: 9 },
     { month: 12, date: 25 },
 ];
+
+export const END_OF_MONTH = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+};
