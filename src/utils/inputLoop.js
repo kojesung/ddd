66cd0bb7dad8eeb7602abc7f05e43b9ebfa11335
kojesung <js95112345@gmail.com>
2026-01-07 16:42:02 +0900

    feat(Input): 근무자 입력에 대해 재입력 처리

--- src/utils/inputLoop.js
+++ src/utils/inputLoop.js
@@ -1,8 +1,24 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 export const inputLoop = async (inputFn, validator) => {
     while (true) {
         try {
             const input = await inputFn();
             return validator(input);
+        } catch (err) {
+            MissionUtils.Console.print(err.message);
+        }
+    }
+};
+
+export const workerInputLoop = async (weekDayInputFn, weekEndInputFn, validator) => {
+    while (true) {
+        try {
+            const weekday = await weekDayInputFn();
+            const weekend = await weekEndInputFn();
+            const parsedWeekday = validator(weekday);
+            const parsedWeekend = validator(weekend);
+            return { weekday: parsedWeekday, weekend: parsedWeekend };
         } catch (err) {
             MissionUtils.Console.print(err.message);
         }
