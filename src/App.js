efb62b1ba577b93ef6d944bb2cc9b9fa074cef6a
kojesung <js95112345@gmail.com>
2026-01-07 18:26:14 +0900

    feat(App): 클래스별 필요한 메서드 조립

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { inputLoop, workerInputLoop } from './utils/inputLoop';
-import InputView from './InputView';
-import Parser from './Parser';
+import { inputLoop, workerInputLoop } from './utils/inputLoop.js';
+import InputView from './InputView.js';
+import Parser from './Parser.js';
+import Calendar from './Calendar.js';
+import OutputView from './OutputView.js';
 
 class App {
     async run() {
-        const { month, day } = await inputLoop(InputView.inputMonthDay, Parser.monthDayParser);
-        const { weekdayWorker, weekendWorker } = workerInputLoop(
+        const { month, startDay } = await inputLoop(InputView.inputMonthDay, Parser.monthDayParser);
+        const { weekdayWorker, weekendWorker } = await workerInputLoop(
             InputView.inputWeekDayWorkerList,
             InputView.inputWeekEndWorkerList,
             Parser.workerListParser
         );
+        const calendar = new Calendar(month, startDay, weekdayWorker, weekendWorker);
+        calendar.setWorker();
+        const workerList = calendar.getWorkerList();
+        OutputView.printWorkerList(workerList);
     }
 }
 
--- src/utils/isNameValidLength.js
+++ src/utils/isNameValidLength.js
@@ -1,4 +1,4 @@
-import { MAX_WORKER_NAME } from '../constants';
+import { MAX_WORKER_NAME } from '../constants.js';
 
 export const isNameValidLength = (name) => {
     return name.length <= MAX_WORKER_NAME;
