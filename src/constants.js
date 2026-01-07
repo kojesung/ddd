a9d83fc02ee1c84d7e84c27c08dace31d51737e6
kojesung <js95112345@gmail.com>
2026-01-07 15:52:14 +0900

    feat(constants): 요일 index 및 검증에 필요한 항목들 상수화

---
+++ src/constants.js
@@ -0,0 +1,32 @@
+export const INDEX_TO_DAY = {
+    0: '일',
+    1: '월',
+    2: '화',
+    3: '수',
+    4: '목',
+    5: '금',
+    6: '토',
+};
+
+export const DAY_TO_INDEX = {
+    일: 0,
+    월: 1,
+    화: 2,
+    수: 3,
+    목: 4,
+    금: 5,
+    토: 6,
+};
+
+export const MAX_WORKER_NAME = 5;
+
+export const PUBLIC_HOLIDAY = [
+    { month: 1, date: 1 },
+    { month: 3, date: 1 },
+    { month: 5, date: 5 },
+    { month: 6, date: 6 },
+    { month: 8, date: 15 },
+    { month: 10, date: 3 },
+    { month: 10, date: 9 },
+    { month: 12, date: 25 },
+];
