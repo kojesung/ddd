f62edbc507b93fa815435af80bd16b0e73c3a50c
kojesung <js95112345@gmail.com>
2026-01-07 18:32:43 +0900

    fix(Parser): 일요일의 index인 0이 들어갔을 때 false가 나오는 문제 수정

--- src/Parser.js
+++ src/Parser.js
@@ -10,7 +10,8 @@ class Parser {
         const parsedMonth = Number(month.trim());
         if (parsedMonth < 1 || parsedMonth > 12) throw new Error('[ERROR] 월의 범위는 1월과 12월 사이로 설정해주세요');
         const dayIndex = DAY_TO_INDEX[day];
-        if (!dayIndex) throw new Error('[ERROR] 시작 요일은 일요일 ~ 토요일 사이의 유효한 요일로 설정해주세요');
+        if (dayIndex === undefined)
+            throw new Error('[ERROR] 시작 요일은 일요일 ~ 토요일 사이의 유효한 요일로 설정해주세요');
         return { month: parsedMonth, startDay: dayIndex };
     }
     static workerListParser(input) {
